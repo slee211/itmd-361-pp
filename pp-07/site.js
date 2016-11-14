9b3f9b96ba77047b48fab0ca686d670372cc5ff6
Seyeon Lee <slee211@hawk.iit.edu>
2016-11-14 10:22:47 -0600

    pp-07

--- pp-07/site.js
+++ pp-07/site.js
@@ -3,7 +3,7 @@
   outputs the correct value for x (5).
 */
 
-var x = 5;
+
 
 function double(num) {
   x = num * 2;
@@ -11,6 +11,7 @@ function double(num) {
 }
 
 double(6);
+var x=5;
 console.log('The value of x is:', x, 'It should be 5.');
 
 /*
@@ -19,3 +20,10 @@ console.log('The value of x is:', x, 'It should be 5.');
   variables nor the double function can be accessed via the
   `window` global object, like `window.x`).
 */
+(function(){
+	function double(num) {
+  x = num * 2;
+  return x;
+}
+console.log('The value of x is:', x, 'It should be 5.');
+}());
